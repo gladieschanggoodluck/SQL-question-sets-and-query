e0780abc308d76f2409e290ea22ba9771ccdf41d
Gladies <49598326+gladieschanggoodluck@users.noreply.github.com>
2020-12-30 11:14:47 -0800

    Update NVL.sql

--- NVL_NULLIF_Coalesce/NVL.sql
+++ NVL_NULLIF_Coalesce/NVL.sql
@@ -26,3 +26,53 @@ NVL(Country, 'No country') "Country",
 NVL(employees, 0) "Employees",
 NVL(start_date, TO_DATE('01-JAN-2000')) 'start_date'
 FROM nvl_customers;
+                        
+                        
+## ISNULL and  NULLIF differences
+## ISNULL : replaces NULL with the specified replaacement value
+## ISNULL (<check expression>, (dispplaced expression)
+ISNULL(Param1, Param2)
+-- IF Param1 = NULL, return Param2
+-- IF Param1 <> Null, return Param1
+-- IF Param1 ISNULL, then return Param2
+
+-- EXAMPLE
+DECLARE @ PARAM1 INT,
+		@ PARAM2 INT
+SELECTT @PARAM1 = 10 @PARAM2 = 20
+SELECT ISNULL(@PARAM1, @PARAM2) 
+# This function returns 10
+		
+ISNULL(NULL, 20)
+# This function returns 20
+
+## NULLIF
+## Retrun a null value if the two specified expressions are equal
+## NULLIF(<Expression1>, <Expresssion2>)
+
+NULLIF(Param1, Param2)
+--If Param1 = Param2, return NULL
+--If Param1 <> Param2, return Param1
+-- EXAMPLE
+
+DECLARE @PARAM1 INT,
+		@PARAM2 INT
+SELECT @ PARAM1 = 10, @PARAM2 = 10
+SELECT NULLIF(@PARAM1, @PARAM2)
+-- RETURN NULL
+
+
+DECLARE @PARAM1 INT,
+		@PARAM2 INT
+SELECT @ PARAM1 = NULL, @PARAM2 = NULL
+SELECT NULLIF(@PARAM1, @PARAM2)
+-- RETURN NULL
+
+
+DECLARE @PARAM1 INT,
+		@PARAM2 INT
+SELECT @ PARAM1 = 10, @PARAM2 = 20
+SELECT NULLIF(@PARAM1, @PARAM2)
+-- RETURN 20
+
+# - DIFFERENCES between ISNULL AND NULLIF
