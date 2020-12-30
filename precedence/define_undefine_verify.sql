b333e2bb884be05efbc1031e5cbf2b3103b49f50
Gladies <49598326+gladieschanggoodluck@users.noreply.github.com>
2020-12-30 10:48:32 -0800

    Update define_undefine_verify.sql

--- precedence/define_undefine_verify.sql
+++ precedence/define_undefine_verify.sql
@@ -16,3 +16,24 @@ DEFINE MY_SALARY = 140000;
 SELECT * FROM employees WHERE salary >= &MY_SALARY;
 
 UNDEFINE MY_SALARY
+
+SELECT * FROM employees WHERE salary >= &MY_SALARY;
+
+UNDEFINE MY_SALARY
+
+## Applying Rules of precedence for operators in an expression 
+## Precedence rule
+sal *100/5 * 25 (10+2) * 5/20
+
+# Oracle SQL precendence rule
+# () - bracket solved first
+# /, * - this is the same as math
+# +, -
+# || Concat
+# =, <, >, <=, >=
+# NULL, NOT NULL
+# BETWEEN 
+# !=,  <>
+# NOT
+# AND
+# OR 
